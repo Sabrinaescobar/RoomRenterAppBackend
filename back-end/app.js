d9d85e5f9ddd023942d8ed8d1fc7ec5823d77745
sabrina <sabrinaescobarflores@gmail.com>
2022-09-11 15:17:51 -0400

    change app.js

--- back-end/app.js
+++ back-end/app.js
@@ -4,19 +4,18 @@ const express = require("express");
 
 // CONFIGURATION
 const app = express();
-
+const apartmentsController = require("./controllers/apartmentsRenterController.js");
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());
+
+// Apartments ROUTES
+app.use("/apartments", apartmentsController);
 
 // ROUTES
 app.get("/", (req, res) => {
   res.send("Welcome to Apartments and Rooms Renter App");
 });
-
-// Apartments ROUTES
-const apartmentsController = require("./controllers/apartmentsRenterController.js");
-app.use("/apartments", apartmentsController);
 
 // 404 PAGE
 app.get("*", (req, res) => {
